72ae6832658fc3dcc92138f78685068c24396d41
任姝乐 <15035111789@163.com>
2018-11-09 16:14:25 +0800

    no message

--- src/main/java/com/weixin/bean/Classify.java
+++ src/main/java/com/weixin/bean/Classify.java
@@ -7,15 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-class User{
-    private Integer u_id;
-    private String u_name;
-    private String u_password;
-}
-
-
-
-
 public class Classify {
     private Integer cId;
     private String cName;
