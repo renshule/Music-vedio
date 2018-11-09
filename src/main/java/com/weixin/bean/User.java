c53f81192b37b684e4b6c59234d1becc702dc1e7
郭蕊妮 <1215106619@qq.com>
2018-11-09 15:51:46 +0800

    文件配置加修改

---
+++ src/main/java/com/weixin/bean/User.java
@@ -0,0 +1,20 @@
+package com.weixin.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class User {
+    private Interger u_id;
+    private String u_name;
+    private String u_password;
+
+
+
+
+}
